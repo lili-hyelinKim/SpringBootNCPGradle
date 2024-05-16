81b9067a81ddea8f27f05d90b4cc4cdb2b3741af
hyelinkim <lins26@naver.com>
2024-05-16 17:41:48 +0900

    1

---
+++ src/main/front/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <h1>Spring Boot + React + NCP TEST</h1>
+      <img src='./image/db.jpg' alt='Bbing_bong'/>
+    </div>
+  );
+}
+
+export default App;
